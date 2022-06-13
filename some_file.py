a97e66deb667076c303efc572a5081abf39f55ae
Rich Ormiston <rich.ormiston@mbp.local>
2022-06-13 18:31:40 -0500

    creating multiple merge conflicts

--- some_file.py
+++ some_file.py
@@ -1,2 +1,16 @@
 print("this is a new file from branch newfix")
 print("resolving conflicts")
+nums = [x for x in range(25) if x%2 == 0]
+
+
+
+
+# This is a comment
+
+
+
+
+
+hw = "hello world"
+for ii in nums:
+    print("saying {}".format(hw))
