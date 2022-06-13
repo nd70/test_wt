a69af0413d971a1cfac010109dad5784ee6b679d
Rich Ormiston <rich.ormiston@mbp.local>
2022-06-13 18:34:10 -0500

    Updating behind master to create conflicts

--- some_file.py
+++ some_file.py
@@ -1,2 +1,12 @@
 print("this is a new file from branch newfix")
 print("resolving conflicts")
+
+nums = list(range(25))
+
+# This is a comment
+
+
+
+
+for x in range(nums):
+    print("{}) resolving multiple merge conflicts".format(x))
